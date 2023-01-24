0669222fe06589e309aad7441f53e5e90b90d2b6
Pavel Liavitski <pashalevy@gmail.com>
2023-01-24 21:19:30 +0100

    Sole and Ankle project addeded

--- sole-and-ankle-revisited/src/components/App/App.js
+++ sole-and-ankle-revisited/src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
+
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
