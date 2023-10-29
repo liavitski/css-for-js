feff26b70a11b36809efcbc969741e0d6b39e929
Pavel Liavitski <pashalevy@gmail.com>
2023-10-29 10:31:37 +0100

    added ProgressBar

--- mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -9,17 +9,17 @@ const STYLES = {
   small: {
     height: 8,
     padding: 0,
-    radius: 4,
+    borderRadius: 4,
   },
   medium: {
     height: 12,
     padding: 0,
-    radius: 4,
+    borderRadius: 4,
   },
   large: {
     height: 16,
     padding: 4,
-    radius: 8,
+    borderRadius: 8,
   },
 };
 
@@ -27,50 +27,52 @@ const ProgressBar = ({ value, size }) => {
   const styles = STYLES[size];
 
   if (!styles) {
-    throw new Error(`Unknown size passed to PrigressBar: ${size}`);
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
   }
 
   return (
-    <Wrapper
-      role="progressbar"
-      aria-valuenow={value}
-      aria-valuemin="0"
-      aria-valuemax="100"
-      style={{
-        '--padding': styles.padding + 'px',
-        '--radius': styles.radius + 'px',
-      }}
-    >
-      <VisuallyHidden>{value}%</VisuallyHidden>
-      <BarWrapper>
-        <Bar
-          style={{
-            '--width': value + '%',
-            '--height': styles.height + 'px',
-          }}
-        />
-      </BarWrapper>
-    </Wrapper>
+    <>
+      <Wrapper
+        role="progressbar"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        style={{
+          '--padding': styles.padding + 'px',
+          '--borderRadius': styles.borderRadius + 'px',
+        }}
+      >
+        <VisuallyHidden>{value}</VisuallyHidden>
+        <BarWrapper>
+          <Bar
+            style={{
+              '--width': value + '%',
+              '--height': styles.height + 'px',
+            }}
+          />
+        </BarWrapper>
+      </Wrapper>
+    </>
   );
 };
 
 const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: var(--radius);
+  border-radius: var(--borderRadius);
+  box-shadow: 0px 2px 4px ${COLORS.transparentGray35} inset;
   padding: var(--padding);
 `;
 
 const BarWrapper = styled.div`
-  /* Trim off corners when progress bar near-full */
+  /* Trim off corners when progress bar is near full. */
   overflow: hidden;
   border-radius: 4px;
 `;
 
 const Bar = styled.div`
-  width: var(--width);
-  height: var(--height);
   background-color: ${COLORS.primary};
+  height: var(--height);
+  width: var(--width);
   border-radius: 4px 0 0 4px;
 `;
 
--- mini-component-library/src/components/Select/Select.js
+++ mini-component-library/src/components/Select/Select.js
@@ -9,62 +9,10 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper>
-      <NativeSelect value={value} onChange={onChange}>
-        {children}
-      </NativeSelect>
-      <PresentationalBit>
-        {displayedValue}{' '}
-        <IconWrapper style={{ '--size': 24 + 'px' }}>
-          <Icon id="chevron-down" strokeWidth={1} size={24} />
-        </IconWrapper>
-      </PresentationalBit>
-    </Wrapper>
+    <select value={value} onChange={onChange}>
+      {children}
+    </select>
   );
 };
-
-const Wrapper = styled.div`
-  position: relative;
-  width: max-content;
-`;
-
-const NativeSelect = styled.select`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  appearance: none;
-`;
-
-const PresentationalBit = styled.div`
-  padding: 12px 16px;
-  background-color: ${COLORS.transparentGray15};
-  color: ${COLORS.gray700};
-  font-size: ${16 / 16}rem;
-  padding-right: 52px;
-  border-radius: 8px;
-
-  ${NativeSelect}:focus + & {
-    outline: 1px dotted #212121;
-    outline: 5px auto -webkit-focus-ring-color;
-  }
-
-  ${NativeSelect}:hover + & {
-    color: ${COLORS.black};
-  }
-`;
-
-const IconWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 10px;
-  margin: auto;
-  height: var(--size);
-  width: var(--size);
-  pointer-events: none;
-`;
 
 export default Select;
