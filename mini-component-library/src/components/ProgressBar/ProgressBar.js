b5e842258fc35060474b1cb34d7d21bad4b08ceb
Pavel Liavitski <pashalevy@gmail.com>
2025-10-07 19:04:09 +0200

    reset module 3

--- mini-component-library/src/components/ProgressBar/ProgressBar.js
+++ mini-component-library/src/components/ProgressBar/ProgressBar.js
@@ -5,70 +5,8 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const STYLES = {
-  small: {
-    height: 8,
-    padding: 0,
-    radius: 4,
-  },
-  medium: {
-    height: 12,
-    padding: 0,
-    radius: 4,
-  },
-  large: {
-    height: 24,
-    padding: 4,
-    radius: 8,
-  },
-};
-
 const ProgressBar = ({ value, size }) => {
-  const styles = STYLES[size];
-
-  if (!styles) {
-    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
-  }
-  return (
-    <Wrapper
-      role="progressbar"
-      aria-valuenow={value}
-      aria-valuemin="0"
-      aria-valuemax="100"
-      style={{
-        '--padding': styles.padding + 'px',
-        '--radius': styles.radius + 'px',
-      }}
-    >
-      <VisuallyHidden>{value}%</VisuallyHidden>
-      <Decorator>
-        <Bar
-          style={{
-            '--progress': value + '%',
-            '--height': styles.height + 'px',
-          }}
-        ></Bar>
-      </Decorator>
-    </Wrapper>
-  );
+  return <strong>{value}</strong>;
 };
-
-const Wrapper = styled.div`
-  background-color: ${COLORS.transparentGray15};
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  padding: var(--padding);
-  border-radius: var(--radius);
-`;
-
-const Decorator = styled.div`
-  border-radius: 4px;
-  overflow: hidden;
-`;
-
-const Bar = styled.div`
-  height: var(--height);
-  width: var(--progress);
-  background-color: ${COLORS.primary};
-`;
 
 export default ProgressBar;
