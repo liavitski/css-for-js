0dc144d8b95edd153d1811d13cb0c39151558c9a
Pavel Liavitski <pashalevy@gmail.com>
2025-10-08 21:06:43 +0200

    select-component done

--- mini-component-library/src/components/Select/Select.helpers.js
+++ mini-component-library/src/components/Select/Select.helpers.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export function getDisplayedValue(value, children) {
   const childArray = React.Children.toArray(children);
+
   const selectedChild = childArray.find(
     (child) => child.props.value === value
   );
--- mini-component-library/src/components/Select/Select.js
+++ mini-component-library/src/components/Select/Select.js
@@ -9,10 +9,63 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  -webkit-appearance: none;
+  appearance: none;
+`;
+
+const PresentationBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  padding-right: 52px;
+  font-size: ${16 / 16}rem;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`;
 
 export default Select;
